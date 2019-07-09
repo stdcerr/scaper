6163fa5446481d7a22843d74d7bc77d4f30f5299
ron <ron@ron-notebook2.localdomain>
2019-07-09 03:35:33 -0700

    removal of build/

--- build/CMakeFiles/scaper_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "scaper_automoc.cpp"
-  "CMakeFiles/scaper_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/scaper_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
