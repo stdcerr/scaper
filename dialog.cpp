5b4a37f1698e9417a3a0069f7b193e5552170335
ron <ron.eggler@gmail.com>
2019-07-09 20:26:30 -0700

    fixed a few things to at least make  it compilable

---
+++ dialog.cpp
@@ -0,0 +1,11 @@
+#include <QtGui>
+#include <dialog.h>
+
+
+ScaperDialog::ScaperDialog(QWidget *parent) {
+
+
+}
+
+void ScaperDialog::CheckAll(void){}
+void ScaperDialog::UncheckAll(void){}
---
+++ dialog.h
@@ -0,0 +1,28 @@
+#ifndef __DIALOG_H__
+#define __DIALOG_H__
+
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QPushButton>
+
+class ScaperDialog: public QDialog
+{
+    Q_OBJECT
+    public:
+        ScaperDialog(QWidget *parent = 0);
+
+        ScaperDialog(){}
+
+    private slots:
+        void CheckAll();
+        void UncheckAll();
+
+    private:
+        QListWidget *list;
+        QCheckBox *chkall;
+        QCheckBox *unchkall;
+        QPushButton *closeBtn;
+};
+
+#endif
